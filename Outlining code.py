8b3aa2725357fe621d2f87a68eaa6bf8fb577d5f
DrChapman <kyle.chapman.topology@gmail.com>
2025-10-14 14:58:12 -0700

    Add files via upload

---
+++ Outlining code.py
@@ -0,0 +1,21 @@
+class to_do_list:
+    def _init_(self):
+        self.list = [] #list of tasks. task is task = (string, boolean, int)
+
+        #maybe add class for tasks with self.start day or self.repetition_interval
+
+    def add_tasks(self,task_name): #lets user add tasks to list.  
+        return None
+
+    def remove_tasks(self,task_name): #lets user remove tasks from list
+        return None
+
+    def check_tasks(self,task_name): #checks off completed tasks
+        #returns True or False boolean
+        return None
+
+    def repeated_tasks(self,task_name): #allows user to choose tasks to repeat
+                                
+        def interval(self):
+        #choose the interval of repetition 
+           return None
