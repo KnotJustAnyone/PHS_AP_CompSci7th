baa6a5ed138229b2192a8b2a3f721fac6b02b15d
Half2beC00L <166662851+Half2beC00L@users.noreply.github.com>
2025-10-30 09:48:32 -0700

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -65,20 +65,21 @@ class Player: #player properties
 
     def doubledown(self):
         currenttot = 0
-        for i in self.hand:
-            currenttot += card_value(i)
-        if currenttot == 9 or currenttot == 10 or currenttot == 11:
-            while True:
-                ifdouble = input(f"Would {self.name} like to double down? (y or n)? ").strip().lower()
-                if ifdouble in ("y", "n"):
-                    break
-                print("y or n please")
-            if ifdouble == "y":
-                self.newcard(1)
-                self.bet = self.bet * 2
-                return True
+        if len(self.hand) == 2:
+            for i in self.hand:
+                currenttot += card_value(i)
+            if currenttot == 9 or currenttot == 10 or currenttot == 11:
+                while True:
+                    ifdouble = input(f"Would {self.name} like to double down? (y or n)? ").strip().lower()
+                    if ifdouble in ("y", "n"):
+                        break
+                    print("y or n please")
+                if ifdouble == "y":
+                    self.newcard(1)
+                    self.bet = self.bet * 2
+                    return True
+            return None
         return None
-
                 
     
 class Dealer: #dealer properties
@@ -178,4 +179,3 @@ def doubledowncheck():
     player.doubledown()
     print(f"Player {player}'s hand: {player.hand}, the bet: {player.bet}")
     print("New hand should have an extra card, net bet should be double the bet.")
-
