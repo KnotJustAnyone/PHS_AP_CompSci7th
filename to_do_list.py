cf5b511b941edd4784d9e905e51a9fda66fad548
DanielleChoi <166639906+DanielleChoi@users.noreply.github.com>
2025-11-13 14:48:19 -0800

    fixed indentation for remove task test

--- to_do_list.py
+++ to_do_list.py
@@ -10,21 +10,6 @@ class to_do_list:
     def remove_tasks(self,task_name): #lets user remove tasks from list
         return None
 
-        def removeTask_test():
-            taskList = ["Do math homework", "wash dishes", "walk the dog"] #the to do list
-            To_be_removed = "wash dishes"
-            final_list = ["Do math homework", "walk the dog"]
-
-            action = "remove"
-            remove_which_task  = 2 #remove wash dishes
-            a = int(remove_which_task) - 1 #making it work with the index number
-            removing = taskList.pop(a)
-
-            if removing == To_be_removed and taskList == final_list:
-                print("test works")
-            else:
-                print ("test failed")
-
     def check_tasks(self,task_name): #checks off completed tasks
         #returns True or False boolean
         return None
@@ -34,3 +19,18 @@ class to_do_list:
         def interval(self):
         #choose the interval of repetition 
            return None
+
+def removeTask_test():
+    taskList = ["Do math homework", "wash dishes", "walk the dog"] #the to do list
+    To_be_removed = "wash dishes"
+    final_list = ["Do math homework", "walk the dog"]
+
+    action = "remove"
+    remove_which_task  = 2 #remove wash dishes
+    a = int(remove_which_task) - 1 #making it work with the index number
+    removing = taskList.pop(a)
+
+    if removing == To_be_removed and taskList == final_list:
+        print("test works")
+    else:
+        print ("test failed")
