9cf41aae7cf57aafcc8b71907cb13b65c566577b
Colin Sweeney2 <colinsweeney2@Colins-MacBook-Air.local>
2025-11-14 12:57:21 -0800

    Fix Player class and enhance poker_table __init__ with configuration options

--- poker_table_class.py
+++ poker_table_class.py
@@ -4,12 +4,12 @@ from collections import Counter
 from itertools import combinations
 
 class Player: #player properties
-    def __init__(self,name,money=1500): #creating player, give money
+    def __init__(self, name, money=1500): #creating player, give money
         self.name = name #player name, may not use because they'll see each other's cards?
         self.hand = [] #hand of cards
         self.money = money #money amount
         self.bet = 0 #Money in pot
-
+        
     def newcard(self, count): #putting card in hand
         self.hand += Deck.deal(count)
         
@@ -17,14 +17,40 @@ class Player: #player properties
         self.hand = []
 
 class poker_table:
-    def __init__(self):
-        self.players = [] #List of players, need a player class
+    def __init__(self, players=None, small_blind=1, big_blind=2, ante=0,
+                button_index=0, max_players=9, starting_stack=None,
+                codes=True, return_not_print=True):
+        # Players and seating
+        self.players = list(players) if players else [] #List of players, need a player class
+        self.max_players = max_players
+
+        # Stakes
+        self.small_blind = small_blind
+        self.big_blind = big_blind if big_blind >= small_blind else small_blind * 2
+        self.ante = ante
+
+        # Pots and betting
         self.pot = 0
         self.bets = []
-        self.deck = Deck(False,True,True)
+
+        # Deck options
+        self.deck = Deck(False, codes, return_not_print, 1)
+
+        # Table and position
         self.table_cards = []
-        self.current_player = None
-        self.button_player = None
+        if self.players:
+            button_index = button_index % len(self.players)
+            self.button_player = self.players[button_index]
+            self.current_player = self.players[(button_index + 1) % len(self.players)]
+        else:
+            self.button_player = None
+            self.current_player = None
+
+        # Chip stacks (rudimentary: equal starting stack for each player if provided)
+        self.stacks = {}
+        if starting_stack is not None and self.players:
+            for p in self.players:
+                self.stacks[p] = starting_stack
 
     def deal_hands(self): #Gives each player their initial two pocket cards
         return None
