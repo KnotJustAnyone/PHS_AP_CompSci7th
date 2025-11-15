70fbba292660dcba78d86b6a493bc36f40c9dd70
Coin333 <116315051+Coin333@users.noreply.github.com>
2025-11-15 13:12:15 -0800

    Test for Add Task and List Task Functions

--- to_do_list.py
+++ to_do_list.py
@@ -32,3 +32,42 @@ class to_do_list:
         def interval(self):
         #choose the interval of repetition 
            return None
+class to_do_list:
+    def __init__(self):
+        self.list = [] #list of tasks. task is task = (string, boolean, int)
+
+        #maybe add class for tasks with self.start day or self.repetition_interval
+
+    def add_tasks(self,task_name): #lets user add tasks to list.  
+        task = (task_name, False, 0)
+        self.list.append(task)
+        print(f"Task '{task_name}' added.")
+
+    def list_tasks(self):
+        if not self.list:
+            print("You have no tasks!")
+        else:
+            print("Your tasks:")
+            for task in self.list:
+                name = task[0]
+                completed = task[1]
+                status = "Done" if completed else "Not done"
+                print(f"- {name} [{status}]")
+
+    def remove_tasks(self,task_name): #lets user remove tasks from list
+        return None
+
+    def check_tasks(self,task_name): #checks off completed tasks
+        #returns True or False boolean
+        return None
+
+    def repeated_tasks(self,task_name): #allows user to choose tasks to repeat
+                                
+        def interval(self):
+        #choose the interval of repetition 
+           return None
+def addandlisttest():
+    dog = to_do_list()
+    dog.add_tasks("Do dishes")
+    dog.list_tasks()
+addandlisttest()
