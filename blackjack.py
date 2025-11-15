855a8cec4cc4564c56709c35db3ed5a016de7632
Half2beC00L <166662851+Half2beC00L@users.noreply.github.com>
2025-11-14 17:39:43 -0800

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -319,11 +319,3 @@ def test_deal1():
             print(f"ERROR ###########\ndealer.deal1() dealt the following cards: {player.hand}, one of which's value could not be determined by card_value()")
     if not errorOccurred:
         print("dealer.deal1 passed all tests")
-
-
-
-
-
-
-
-
