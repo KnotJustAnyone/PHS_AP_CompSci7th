6c36baa58b68c184c57993e5cc12075d00b57e73
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-12-05 14:46:25 -0800

    fix space vs tab

--- hangman.py
+++ hangman.py
@@ -117,11 +117,11 @@ def run_game():
     game = WordGuessingGame()
 	game.choose_word()
 
-    while True: # Loop in case of restarts
-        while True: # Main game loop
-            game.update_display() # Redundancy? Will need fixing upon function coding
-            game.show_status() # Redundancy? Will need fixing upon function coding
-            if game.game_over:
+	while True: # Loop in case of restarts
+		while True: # Main game loop
+			game.update_display() # Redundancy? Will need fixing upon function coding
+			game.show_status() # Redundancy? Will need fixing upon function coding
+			if game.game_over:
                 break
 
     # Restart?
@@ -136,6 +136,7 @@ def run_game():
             quit()
 
 run_game()
+
 
 
 
