e80188b053375005741507f43f2649fb4d5bd0a1
DrChapman <kyle.chapman.topology@gmail.com>
2026-03-27 13:51:05 -0700

    Basic Board Maker

--- RicochetRobots.py
+++ RicochetRobots.py
@@ -7,19 +7,34 @@ The goal is to move the robots to a target location in the fewest number of stop
 #  |  |  Maze format
 #--####
 '''
-class richocet_board:
+class ricochet_board:
+    space = ['  ']
+    vline = ['|']
+    hline = ['--']
+    cross = ['+']
+    board = None
 
     def __init__(self):
-        pass
+        Wall_line = '#'*65
+        Space_line = '#'+("   |")*15+'   #'
+        Grid_line = '#'+'---+'*15+'---#'
+        self.board = [Wall_line]+[Space_line,Grid_line]*15+[Space_line,Wall_line]
+        
+    def print_board(self):
+        for row in self.board:
+            print(row)
         
 
 def menu():
     choice = None
     while choice != '2':
-        choice = input("1) Start a new game\n2) Quit\n")
+        choice = input("1) Start a new game\n2) Quit\nD) Debug\n")
         if choice == '1':
             print("Game still in development")
-        if choice not in ['1','2']:
+        if choice == 'D':
+            test_board = ricochet_board()
+            test_board.print_board()
+        if choice not in ['1','2','D']:
             print("Invalid Choice")
     print("Thanks for Playing")
 
